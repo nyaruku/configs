0da725f8866d6243d61a25b18c0d3d73da6781ee
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-08-26 08:45:30 +0200

    first_install.sh: proper bashrc symlinking

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,1 +1,17 @@
-/home/railgun/.bashrc
+#
+# ~/.bashrc
+# Default Bash Config
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls -a --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+
+#
+# Custom Bash Config 
+#
+
+alias ll='ls -lha --color=auto'
--- scripts/first_install.sh
+++ scripts/first_install.sh
@@ -1,22 +1,60 @@
 #!/bin/bash
 
-sudo pacman -S --needed firefox nano vim git base-devel tmux ntfs-3g os-prober keepassxc hyfetch fastfetch less
+# VARIABLES
+USER="railgun"
+REPO_PATH="/home/$USER/configs"
+BASHRC_PATH="/home/$USER/.bashrc"
 
-# Load bash config if it exists
-if [ -f ../bash/.bashrc ]; then
-    source ../bash/.bashrc
-else
-    echo "Warning: ~/.bashrc not found!"
-fi
 
-# create git folder for git clones
-mkdir -p ~/git
+
+# COLORED ECHO OUTPUT FUNCTIONS
+echo_blue() {
+    echo -e "\033[1;34m$1\033[0m"
+}
+
+
+
+echo_blue "Installing packages..."
+sudo pacman -S --needed firefox nano vim git base-devel tmux ntfs-3g os-prober keepassxc hyfetch fastfetch less dpkg
+
+
+
+# Create symlink and load .bashrc config
+echo_blue "Deleting .bashrc from ~ and symlink to the repo's config"
+rm "$BASHRC_PATH"
+ln -sf "$REPO_PATH/bash/.bashrc" "$BASHRC_PATH"
+echo_blue "Created symlink, loading config..."
+source "$BASHRC_PATH"
+
+
+
+# create directories
+echo_blue "Creating directories..."
+mkdir -p ~/git  ~/deb
+
+
 
 # Template
 # git clone --branch <package_name> --single-branch https://github.com/archlinux/aur.git
 
-echo "Cloning yay from AUR..."
 
 
-mkdir -p ~/git/yay
-git clone --branch yay --single-branch https://github.com/archlinux/aur.git ~/git/yay && cd ~/git/yay && makepkg -si
+echo_blue "Cloning yay from AUR..."
+if [ -d ~/git/yay ]; then
+    echo_blue "~/git/yay already exists"
+    
+    if command -v yay >/dev/null 2>&1; then
+        echo_blue "yay is installed"
+    else
+        echo_blue "yay is not installed, attempting to clone and build..."
+        git clone --branch yay --single-branch https://github.com/archlinux/aur.git ~/git/yay && cd ~/git/yay && makepkg -si
+    fi
+   
+else
+    echo_blue "~/git/yay not found"
+    mkdir -p ~/git/yay
+    echo_blue "Created ~/git/yay, cloning and buildung yay..."
+    git clone --branch yay --single-branch https://github.com/archlinux/aur.git ~/git/yay && cd ~/git/yay && makepkg -si
+fi
+
+
